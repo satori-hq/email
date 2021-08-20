9637646ecad9ef1d824364946ad46cacb30db58a
mattlockyer <mattdlockyer@gmail.com>
2021-08-19 17:17:47 -0700

    add babel and dist

--- src/index.js
+++ src/index.js
@@ -192,7 +192,6 @@ export const template = ({
 `
 }
 
-
 // <!-- START FOOTER -->
 // <div class="footer" style="clear: both; Margin-top: 10px; text-align: center; width: 100%;">
 // 	<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
